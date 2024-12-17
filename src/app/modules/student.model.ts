d19f46ff73d3c7b549e05148aa395e04caada5fc
BP Devs <bpdevs1037@gmail.com>
2024-12-18 00:38:28 +0600

    today work done-17-12-2024

--- src/app/modules/student.model.ts
+++ src/app/modules/student.model.ts
@@ -1,83 +1,133 @@
-import { Guardian, LocalGuardian, Student, UserName } from './student/student.interface';
-import { Schema, model} from 'mongoose';
+import {
+  Guardian,
+  LocalGuardian,
+  Student,
+  UserName,
+} from "./student/student.interface";
+import { Schema, model } from "mongoose";
 
-const UserNameSchema = new Schema <UserName>({
-    firstName: {
-        type: String,
-        required : true,
-    },
-    middleName: {
-        type: String,
-    },
-    lastName: {
-        type: String,
-        required: true,
-    }
-})
+const UserNameSchema = new Schema<UserName>({
+  firstName: {
+    type: String,
+    required: [true, "First Name is Required"],
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last Name is Required"],
+  },
+});
 
 const GuardianSchema = new Schema<Guardian>({
-    fatherName:{
-        type: String,
-        required: true,
-    },
-    fatherOccupation: {
-        type: String,
-        required: true, 
-    },
-    fatherContactNo: {
-        type: String,
-        required: true, 
-    },
-    motherName:{
-        type: String,
-        required: true, 
-    },
-    motherOccupation:{
-        type: String,
-        required: true, 
-    },
-    motherContactNo:{
-        type: String,
-        required: true, 
-    }, 
-})
+  fatherName: {
+    type: String,
+    required: [true, "Father Name Is Required"],
+  },
+  fatherOccupation: {
+    type: String,
+    required: [true, "Father Occupation Is Required"],
+  },
+  fatherContactNo: {
+    type: String,
+    required: [true, "Father Contact Number Is Required"],
+  },
+  motherName: {
+    type: String,
+    required: [true, "Mother Name Is Required"],
+  },
+  motherOccupation: {
+    type: String,
+    required: [true, "Mother Occupation Is Required"],
+  },
+  motherContactNo: {
+    type: String,
+    required: [true, "Mother Contact Number Is Required"],
+  },
+});
 
-const LocalGuardianSchema = new Schema<LocalGuardian>(
-    {
-        name:{
-            type: String,
-            required: true, 
-        },
-        occupation:{
-            type: String,
-            required: true, 
-        },
-        contactNo: {
-            type: String,
-            required: true, 
-        },
-        address: {
-            type: String,
-            required: true, 
-        },
-    }
-)
+const LocalGuardianSchema = new Schema<LocalGuardian>({
+  name: {
+    type: String,
+    required: [true, "Local Guardian Is Required"],
+  },
+  occupation: {
+    type: String,
+    required: [true, "Local Guardian Occupation Is Required"],
+  },
+  contactNo: {
+    type: String,
+    required: [true, "Local Guardian Contact Number Is Required"],
+  },
+  address: {
+    type: String,
+    required: [true, "Local Guardian Address Is Required"],
+  },
+});
 
-const studentSchema = new Schema <Student>({
-    id: { type: String },
-    name: UserNameSchema,
-    gender: ['male', 'female'],
-    dateOfBirth: { type: String },
-    email: { type: String, required: true },
-    contactNos:{ type: String, required: true },
-    emergencyContactNo: { type: String, required: true },
-    bloodGroup: ['A+', 'A-', 'AB+', 'AB-', 'B+', 'B-', 'O+', 'O-'],
-    presentAddress:{ type: String, required: true },
-    permanentAddress:{ type: String, required: true },
-    guardian: GuardianSchema,
-    localGuardian: LocalGuardianSchema,
-    profileImage: {type: String},
-    isActive: ['active', 'blocked']
-})
+const studentSchema = new Schema<Student>({
+  id: { type: String, required: true, unique: true },
+  name: {
+    type: UserNameSchema,
+    required: [true, "User Name Is Required"],
+  },
+  gender: {
+    type: String,
+    enum: {
+      values: ["male", "female", "other"],
+      message: `Gender Is Required : "male", "female", "other"`,
+    },
+    required: true,
+  },
+  dateOfBirth: { type: String },
+  email: {
+    type: String,
+    required: [true, "User Email Is Required"],
+    unique: true,
+  },
+  contactNos: {
+    type: String,
+    required: [true, "User Contact Number Is Required"],
+  },
+  emergencyContactNo: {
+    type: String,
+    required: [true, "User Emergency Contact Number Is Required"],
+  },
+  bloodGroup: {
+    type: String,
+    enum: {
+      values: ["A+", "A-", "AB+", "AB-", "B+", "B-", "O+", "O-"],
+      message: `Blood Group is Required : "A+", "A-", "AB+", "AB-", "B+", "B-", "O+", "O-"`,
+    },
+    required: true,
+  },
+  presentAddress: {
+    type: String,
+    required: [true, "User Present Address Is Required"],
+  },
+  permanentAddress: {
+    type: String,
+    required: [true, "User Permanent Address Is Required"],
+  },
+  guardian: {
+    type: GuardianSchema,
+    required: [true, "User Guardian Info Is Required"],
+  },
+  localGuardian: {
+    type: LocalGuardianSchema,
+    required: [true, "User Local Guardian Is Required"],
+  },
+  profileImage: { type: String },
+  isActive: {
+    type: String,
+    enum: {
+      values: ["active", "blocked"],
+      message: "User Status Is Required: 'active', 'blocked'",
+    },
+    default: "active",
+    required: true,
+  },
+});
 
-export const StudentModel = model<Student>('Student', studentSchema);
+export const StudentModel = model<Student>("Student", studentSchema);
--- src/app/modules/student/student.interface.ts
+++ src/app/modules/student/student.interface.ts
@@ -1,40 +1,38 @@
-
-
 export type Guardian = {
-    fatherName: string;
-    fatherOccupation: string;
-    fatherContactNo: string;
-    motherName: string;
-    motherOccupation: string;
-    motherContactNo: string;
-}
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
 
 export type UserName = {
-    firstName: string;
-    middleName: string;
-    lastName: string
-}
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
 
-export type LocalGuardian ={
-    name: string;
-    occupation: string;
-    contactNo: string;
-    address: string;
-}
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
 
 export type Student = {
-    id: string;
-    name: UserName;
-    gender: "male"|"female";
-    dateOfBirth?: string;
-    email: string;
-    contactNos: string;
-    emergencyContactNo: string;
-    bloodGroup?: 'A+'|'A-'|'B+'|'B-'|'AB+'|'AB-'|'O+'|'O-';
-    presentAddress: string;
-    permanentAddress: string;
-    guardian: Guardian;
-    localGuardian: LocalGuardian;
-    profileImage?: string;
-    isActive: 'active'|'blocked';
-  }
+  id: string;
+  name: UserName;
+  gender: "male" | "female" | "other";
+  dateOfBirth?: string;
+  email: string;
+  contactNos: string;
+  emergencyContactNo: string;
+  bloodGroup?: "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-" | "O+" | "O-";
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImage?: string;
+  isActive: "active" | "blocked";
+};
